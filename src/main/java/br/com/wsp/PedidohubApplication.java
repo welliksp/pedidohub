311dcc4e5dd326d1b35254e88a6686c24a751927
Wellik Perroni <welliksp@gmail.com>
2025-07-21 08:13:35 -0300

    feat: initial commit

---
+++ src/main/java/br/com/wsp/PedidohubApplication.java
@@ -0,0 +1,13 @@
+package br.com.wsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidohubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidohubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wsp/PedidohubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidohubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
